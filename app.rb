6b6aaf2dd8d2c4ed5e12c4f26557f42f9ef749f4
Nozim Yusupov <nozimy@yyandex.ru>
2022-01-16 11:41:23 +0500

    Add dateTimePicker plugin

--- app.rb
+++ app.rb
@@ -78,7 +78,7 @@ post '/visit' do
       [@username, @phone, @datetime, 
        @barber, @color]
 
-  erb "Ok, username is {@username}!, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+  erb "Ok, username is #{@username}!, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 end
 
 post '/contacts' do
@@ -101,25 +101,6 @@ post '/contacts' do
       :password             => 'Naut1lu$',
       :authentication       => :plain
  })
-end
-
-get '/login/form' do
-  erb :login_form
-end
-
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from
-end
-
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-get '/secure/place' do
-  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
 end
 
 def get_db
